30e3d64aa9f7413f1609b8d3cda1e41ebb67c5bb
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-15 05:43:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,13 @@
+# Person
+#   is defined within lib/person.rb
+#   variables defined in lib/person.rb
+#     defines a local variable adele_goldberg as an instance of a Person
+#     defines a local variable alan_kay as an instance of a Person
+
+
 class Person
 
-end 
+end
 
-adele_goldberg = Person.new 
-alan_kay = Person.new 
+adele_goldberg = Person.new
+alan_kay = Person.new
